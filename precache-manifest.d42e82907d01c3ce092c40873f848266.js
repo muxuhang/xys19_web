df29a6241c8087d8a1278ce67ad48c0d445284b4
xuhang mu <muxuhang@gmail.com>
2019-10-28 19:16:06 +0800

    Updates

---
+++ precache-manifest.d42e82907d01c3ce092c40873f848266.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef2377ca05ba07d99ef7ced30df3e170",
+    "url": "/xys19_web/index.html"
+  },
+  {
+    "revision": "a5936e6ab81d0cbe1063",
+    "url": "/xys19_web/static/css/main.91c5182c.chunk.css"
+  },
+  {
+    "revision": "7c76707bed845e8ff92b",
+    "url": "/xys19_web/static/js/2.d90128db.chunk.js"
+  },
+  {
+    "revision": "a5936e6ab81d0cbe1063",
+    "url": "/xys19_web/static/js/main.493caec9.chunk.js"
+  },
+  {
+    "revision": "c5475bfa9eee08707cd7",
+    "url": "/xys19_web/static/js/runtime-main.d59e43d1.js"
+  },
+  {
+    "revision": "47cd5699d0e15cbc493f5ed970d5ee56",
+    "url": "/xys19_web/static/media/back.47cd5699.png"
+  },
+  {
+    "revision": "5f82827a1429f0982cc09cdfa9991fc0",
+    "url": "/xys19_web/static/media/icon.5f82827a.png"
+  }
+]);
