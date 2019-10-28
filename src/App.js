4efaa81a47e43ba05d3d9686c0eb4e42611c8791
xuhang mu <muxuhang@gmail.com>
2019-10-28 19:08:18 +0800

    下载页

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+import back from './img/back.png'
+import icon from './img/icon.png'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img className='appBack' alt='' src={back} ></img>
+      <div className='appContainer'>
+        <img className='appIcon' alt='' src={icon}></img>
+        <div className='appTitle'>道 地 易 通</div>
+        <div className='appText'>
+          <span>说明</span>
+          久等了各位，app的测试目前已接近尾声，新改版的APP不久后会在APP Store 与安卓市场上架。 点击下方链接，查看下载链接以及新版app 变动。
+        </div>
+        <div className='appButton appAndroid'>Android版本下载</div>
+        <div className='appButton appIos'>iOS版本下载</div>
+      </div>
     </div>
   );
 }
