707938465d9e67ecaf021484c78214ff3bcfc8c6
xuhang mu <muxuhang@gmail.com>
2019-10-29 09:28:34 +0800

    删除点击提示

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
         <div className='appTitle'>道 地 易 通</div>
         <div className='appText'>
           <span>说明</span>
-          久等了各位，app的测试目前已接近尾声，新改版的APP不久后会在APP Store 与安卓市场上架。 点击下方链接，查看下载链接以及新版app 变动。
+          久等了各位，app的测试目前已接近尾声，新改版的APP不久后会在APP Store 与安卓市场上架。
         </div>
         <div className='appButton appAndroid'>Android版本下载</div>
         <div className='appButton appIos'>iOS版本下载</div>
