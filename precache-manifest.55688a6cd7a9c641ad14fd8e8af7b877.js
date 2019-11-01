c753eb4dd6fcbe9db4f8c0ea944225d31b2ff2e3
xuhang mu <muxuhang@gmail.com>
2019-11-01 10:03:39 +0800

    Updates

---
+++ precache-manifest.55688a6cd7a9c641ad14fd8e8af7b877.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2900f377442ab53a6b1ef5270dcab132",
+    "url": "/index.html"
+  },
+  {
+    "revision": "928ac8a725086c689123",
+    "url": "/static/css/main.f8003a63.chunk.css"
+  },
+  {
+    "revision": "7c76707bed845e8ff92b",
+    "url": "/static/js/2.d90128db.chunk.js"
+  },
+  {
+    "revision": "928ac8a725086c689123",
+    "url": "/static/js/main.8f8b05fd.chunk.js"
+  },
+  {
+    "revision": "811a9c4a007475311930",
+    "url": "/static/js/runtime-main.56a436ab.js"
+  },
+  {
+    "revision": "47cd5699d0e15cbc493f5ed970d5ee56",
+    "url": "/static/media/back.47cd5699.png"
+  },
+  {
+    "revision": "5f82827a1429f0982cc09cdfa9991fc0",
+    "url": "/static/media/icon.5f82827a.png"
+  },
+  {
+    "revision": "96999011f6f83db38b4408ba1215a0b8",
+    "url": "/static/media/xys19_v0.3.5.96999011.apk"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9dede92056ef89a3a39523a8870608f5.js"
+  "/precache-manifest.55688a6cd7a9c641ad14fd8e8af7b877.js"
 );
 
 self.addEventListener('message', (event) => {
